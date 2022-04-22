10064bd3f7fbbd5bc0bdab0c91fea90134e191a3
Ryan Young <ryan.d.young@du.edu>
2022-04-22 02:47:39 -0600

    updates

--- src/chart_tools/datasets/datasets.py
+++ src/chart_tools/datasets/datasets.py
@@ -1,42 +1,83 @@
 import pandas as pd
 import requests
 import json
-from locations import locations
+from chart_tools.datasets.locations import locations
 
-other_names = 'https://api.github.com/repos/ryayoung/datasets/git/trees/main?recursive=1'
+# other_names = 'https://api.github.com/repos/ryayoung/datasets/git/trees/main?recursive=1'
 
-def build_source(key, name):
-    s = locations[key]
-    return "https://raw.githubusercontent.com/{s['u']}/{s['r']}/{s['b']}/{s['l']}{name}.csv"
-
-
-def build_request(key):
-    s = locations[key]
-    return "https://api.github.com/repos/{s['u']}/{s['r']}/git/trees/{s['b']}?recursive=1"
+def build_source(s, name):
+    return f"https://raw.githubusercontent.com/{s['u']}/{s['r']}/{s['b']}/{s['l']}{name}.csv"
 
 
-def load_data(name, source='ryayoung', **kwargs):
-    url = build_source(source, name)
+def build_request(s):
+    return f"https://api.github.com/repos/{s['u']}/{s['r']}/git/trees/{s['b']}?recursive=1"
 
 
+def validate(name, source):
+    # If no name is passed, provide help
+    if not name:
+        print("Call load_data('help') for a live update on all available data sources and their datasets.")
+        print("Call load_data('help', 'source') for a list of sources")
+        print("Call load_data('help', <valid_source>) for a live update on all datasets in a given source")
+        return False
+
+    elif name == 'help' and source == 'source':
+        print("Available data sources:")
+        print("----------------------")
+        print(*locations.keys(), sep="\n")
+        return False
+
+    elif name == 'help':
+        keys = locations.keys() if source == None else [source]
+        if len(keys) == 1:
+            print(f"Available datasets for {source}:")
+        else:
+            print("All available datasets")
+        print("------------------")
+        for source in keys:
+            s = locations[source]
+            url = build_request(s)
+            available = requests.get(url).json()['tree']
+            names = [f"'{f['path'].removesuffix('.csv').removeprefix(s['l'])}'"
+                        for f in available if ".csv" in f['path']]
+            print(f"{source}:\n  ", end="")
+            print(*names, sep="\n  ")
+        return False
+    else:
+        return True
+
+
+def load_data(name=None, source=None, **kwargs):
+    if not validate(name, source):
+        return None
+    
+    if name and source == None:
+        source = 'ryayoung'
+
+    s = locations.get(source, None)
+    if not s:
+        print(f"Unknown source '{source}'. The following are available:")
+        print("-------------------------------------------")
+        print(*locations.keys(), sep=", ")
+        return None
+
+    url = build_source(s, name)
+    
+    # Now we know the source is valid.
+    # Try to fetch the dataset
     try:
         df = pd.read_csv(url, **kwargs)
+        return df
     except Exception as e:
-        s = locations.get(source, None)
-        if s == None:
-            print("Unknown source. The following are available:")
-            print(*locations.keys(), sep=", ")
-            return None
-        else:
-            url = build_request(source)
-            r = requests.get(url)
-            res = r.json()['tree']
-            names = [f"'{f['path'].removesuffix('.csv').removeprefix(s['l'])}'"
-                        for f in res if ".csv" in f['path']]
-            print("Couldn't find dataset. The following are currently available:")
-            print(*names, sep=", ")
-            return None
+        url = build_request(s)
+        r = requests.get(url) # This should work, since source is valid
+        res = r.json()['tree']
+        names = [f"'{f['path'].removesuffix('.csv').removeprefix(s['l'])}'"
+                    for f in res if ".csv" in f['path']]
+        print(f"Couldn't find dataset.\nThe following are currently available for '{source}':")
+        print("-------------------------------------------------------")
+        print(*names, sep=", ")
+        return None
 
-    return df
 
-print(load_data(""))
+# print(load_data(""))
--- src/chart_tools/heatmaps/superheat.py
+++ src/chart_tools/heatmaps/superheat.py
@@ -3,10 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-
-def superheat(corr, title=None, size=13, masked=True, **kwargs):
-    marker = kwargs.get('marker', 's')
-
+def superheat(
+        corr, title=None, mark_scale=5, size=13, masked=True, cbar=True,
+        grid=True, palette=None, marker='s', bar_ticks=5, n_colors=128, **kwargs
+    ):
     # Data
     dfc = corr.copy()
     dfc = dfc.reindex(sorted(dfc.columns), axis=1)
@@ -30,8 +30,11 @@ def superheat(corr, title=None, size=13, masked=True, **kwargs):
     ax.set_title(title)
 
     # Color
-    n_colors = 256
-    palette = sns.diverging_palette(20, 220, n=n_colors)
+    if palette == None:
+        palette = sns.diverging_palette(20, 220, n=n_colors)
+    else:
+        n_colors = len(palette)
+    
     color_min, color_max = [-1, 1]
     def value_to_color(val):
         val_position = float((val - color_min)) / (color_max - color_min) # position of value in the input range, relative to the length of the input range
@@ -47,14 +50,14 @@ def superheat(corr, title=None, size=13, masked=True, **kwargs):
     ymap = y.map(y_to_num)
     
     # Draw
-    size_scale = 500
+    size_scale = mark_scale * 100
     ax.scatter(
+        x=kwargs.pop('x', xmap),
+        y=kwargs.pop('y', ymap),
+        s=kwargs.pop('s', (size * size_scale)), # Vector of square sizes, proportional to size parameter
+        c=kwargs.pop('c', (dfc['value'].apply(value_to_color))),
+        marker=marker, # Use square as scatterplot marker
         **kwargs,
-        x=xmap,
-        y=ymap,
-        s=size * size_scale, # Vector of square sizes, proportional to size parameter
-        c=dfc['value'].apply(value_to_color),
-        marker=marker# Use square as scatterplot marker
     )
     
     # Show column labels on the axes
@@ -63,30 +66,37 @@ def superheat(corr, title=None, size=13, masked=True, **kwargs):
     ax.set_yticks([y_to_num[v] for v in y_labels])
     ax.set_yticklabels(y_labels)
     ax.grid(False, 'major')
-    ax.grid(True, 'minor')
+
+    if grid == True:
+        ax.grid(True, 'minor')
+
     ax.set_xticks([t + 0.5 for t in ax.get_xticks()], minor=True)
     ax.set_yticks([t + 0.5 for t in ax.get_yticks()], minor=True)
     ax.set_xlim([-0.5, max([v for v in x_to_num.values()]) + 0.5])
     ax.set_ylim([-0.5, max([v for v in y_to_num.values()]) + 0.5])
     ax.invert_xaxis()
-    # Color Bar
-    ax = plt.subplot(plot_grid[:,-1])
 
-    col_x = [1]*len(palette)
-    bar_y = np.linspace(color_min, color_max, n_colors)
+    # Color Bar
+    if cbar == True:
+        axb = plt.subplot(plot_grid[:,-1])
+
+        col_x = [1]*len(palette)
+        bar_y = np.linspace(color_min, color_max, n_colors)
+        
+        bar_height = bar_y[1] - bar_y[0]
+        axb.barh(
+            y=bar_y,
+            width=[2]*len(palette), # make bars 5 units wide
+            left=col_x, # Make bars start at 0
+            height=bar_height,
+            color=palette,
+            linewidth=0
+        )
+        axb.set_xlim(1, 2) # Bars are going from 0 to 5, so lets crop the plot somewhere in the middle
+        axb.grid(False) # Hide grid
+        axb.set_facecolor('white') # Make background white
+        axb.set_xticks([]) # Remove horizontal ticks
+        axb.set_yticks(np.linspace(min(bar_y), max(bar_y), bar_ticks)) # Show vertical ticks for min, middle and max
+        axb.yaxis.tick_right() # Show vertical ticks on the right
     
-    bar_height = bar_y[1] - bar_y[0]
-    ax.barh(
-        y=bar_y,
-        width=[2]*len(palette), # make bars 5 units wide
-        left=col_x, # Make bars start at 0
-        height=bar_height,
-        color=palette,
-        linewidth=0
-    )
-    ax.set_xlim(1, 2) # Bars are going from 0 to 5, so lets crop the plot somewhere in the middle
-    ax.grid(False) # Hide grid
-    ax.set_facecolor('white') # Make background white
-    ax.set_xticks([]) # Remove horizontal ticks
-    ax.set_yticks(np.linspace(min(bar_y), max(bar_y), 5)) # Show vertical ticks for min, middle and max
-    ax.yaxis.tick_right() # Show vertical ticks on the right
+    return (fig, ax, axb)
